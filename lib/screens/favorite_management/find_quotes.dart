df7abc868543ef761d570121434634ac9668999b
sanjay8330 <sanjayabi8330@gmail.com>
2022-03-01 21:12:34 +0530

    Created new directories

---
+++ lib/screens/favorite_management/find_quotes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FindQuotes extends StatelessWidget {
+  const FindQuotes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
