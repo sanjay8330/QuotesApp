be3196ff5770d2cd975dd0c919b89a2c8eba5a10
sanjay8330 <sanjayabi8330@gmail.com>
2022-03-11 07:46:08 +0530

    Commit as at 11/03/22 - Completed adding quotes to favorites list

--- lib/components/favorite_management_components/bottom_iconlist.dart
+++ lib/components/favorite_management_components/bottom_iconlist.dart
@@ -1,26 +1,114 @@
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:quotes_app/database_manager/favorite_manager/database_handler_favorites.dart';
 import 'package:quotes_app/screens/favorite_management/view_favorites.dart';
 
-class BottomIconList extends StatelessWidget {
+class BottomIconList extends StatefulWidget {
   final String copyText;
+  final String userID;
 
   const BottomIconList({
     Key? key,
-    required this.copyText
+    required this.copyText,
+    required this.userID,
   }) : super(key: key);
+
+  @override
+  State<BottomIconList> createState() => _BottomIconListState();
+}
+
+class _BottomIconListState extends State<BottomIconList> {
+
+  List userQuoteList = [];
+  List quoteList = [];
+  bool quoteAlreadyExist = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserQuotes();
+  }
+
+  /*
+   * @Developer: Sanjay Sakthivel (IT19158228)
+   * @Created Date: 11/03/2022
+   * @Purpose: This method gets the list of quotes already added to favorites by the user
+   */
+  fetchUserQuotes() async {
+    dynamic result = await DatabaseHandler().getQuotesByUserID(widget.userID.toString());
+
+    //Set the results obtained to userQuotesList
+    setState(() {
+      userQuoteList = result;
+    });
+
+    //Set the values obtained from the quotes list to an independant list
+    //Sample Response -> {quotes: [Early bird catches the worm , No matter what happens try to believe in god]}
+    for (var element in userQuoteList) {
+      setState(() {
+        quoteList = element['quotes'];
+      });
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
 
+    /*
+   * @Developer: Sanjay Sakthivel (IT19158228)
+   * @Created Date: 11/03/2022
+   * @Purpose: This method copies the quote to the clipboard
+   */
     void copyText() {
-      FlutterClipboard.copy(this.copyText).then((value) {
+      FlutterClipboard.copy(widget.copyText).then((value) {
         ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
                 content: Text('Quote Copied To Clipboard')
             )
         );
       });
+    }
+
+    /*
+   * @Developer: Sanjay Sakthivel (IT19158228)
+   * @Created Date: 11/03/2022
+   * @Purpose: This method adds the quote to the favorite list of user
+   */
+    void addToFavorites() async {
+
+      //This checks if the quote selected to add to favorites already exists in the user favorite list
+      for(String singleQuote in quoteList){
+        if(singleQuote == widget.copyText){
+          setState(() {
+            quoteAlreadyExist = true;
+            return;
+          });
+        }
+      }
+
+      if(quoteAlreadyExist){
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Quote exists in Favorites!')
+            )
+        );
+      }else{
+        //Adds the quote if it doesn't exist in the favorite list of user
+        List quoteListToUpdate = [];
+        quoteListToUpdate.add(widget.copyText);
+
+        bool result = await DatabaseHandler().addQuotesForUserID(widget.userID, quoteListToUpdate);
+
+        if(result){
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                  content: Text('Quote Added to Favorites!')
+              )
+          );
+          Navigator.of(context).pushNamed(ViewFavorites.routeName);
+        }
+      }
     }
 
     return Padding(
@@ -31,15 +119,7 @@ class BottomIconList extends StatelessWidget {
           Column(
             children: [
               IconButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text('Quote added to Favorites')
-                        )
-                    );
-
-                    Navigator.of(context).pushNamed(ViewFavorites.routeName);//Maybe Remove with Integration
-                  },
+                  onPressed: addToFavorites,
                   icon: const Icon(
                     Icons.favorite_sharp,
                     color: Colors.red,
