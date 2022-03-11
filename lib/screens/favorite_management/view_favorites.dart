48b13794b1fa801525e3fe1c0b9f507ec751510a
sanjay8330 <sanjayabi8330@gmail.com>
2022-03-11 16:58:07 +0530

    Commit as at 11/03/22 - View favorites according to userID completed.

--- lib/screens/favorite_management/view_favorites.dart
+++ lib/screens/favorite_management/view_favorites.dart
@@ -2,12 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:quotes_app/components/layout.dart';
 import 'package:quotes_app/screens/favorite_management/view_single_quote.dart';
 
-import '../../data/quotes.dart';
+import '../../database_manager/favorite_manager/database_handler_favorites.dart';
 
-class ViewFavorites extends StatelessWidget {
+class ViewFavorites extends StatefulWidget {
   static String routeName = '/viewFavorites';
 
-  const ViewFavorites({Key? key}) : super(key: key);
+  final String? userID;
+  const ViewFavorites({
+    Key? key,
+    this.userID,
+  }) : super(key: key);
+
+  @override
+  State<ViewFavorites> createState() => _ViewFavoritesState();
+}
+
+class _ViewFavoritesState extends State<ViewFavorites> {
+  List userQuoteList = [];
+  List quoteList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserQuotes();
+  }
+
+  /*
+   * @Developer: Sanjay Sakthivel (IT19158228)
+   * @Created Date: 11/03/2022
+   * @Purpose: This method gets the list of quotes already added to favorites by the user
+   */
+  fetchUserQuotes() async {
+    dynamic result = await DatabaseHandler().getQuotesByUserID(widget.userID.toString());
+
+    //Set the results obtained to userQuotesList
+    setState(() {
+      userQuoteList = result;
+    });
+
+    //Set the values obtained from the quotes list to an independant list
+    //Sample Response -> {quotes: [Early bird catches the worm , No matter what happens try to believe in god]}
+    for (var element in userQuoteList) {
+      setState(() {
+        quoteList = element['quotes'];
+      });
+    }
+    print('QUOTES FOR USER>>>>>>'+ quoteList.toString());
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,27 +66,24 @@ class ViewFavorites extends StatelessWidget {
           ),
           Flexible(
             child: ListView.builder(
-                itemCount: quotesList.length,
+                itemCount: quoteList.length,
                 itemBuilder: (context, index) {
-                  Quotes quote = quotesList.elementAt(index);//Need to add a favorites list here
+                  //Quotes quote = quotesList.elementAt(index);//Need to add a favorites list here
                   return Card(
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
                     margin: const EdgeInsets.all(8.0),
                     elevation: 4,
                     child: ListTile(
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(25.0),
-                        child: Image.asset(quote.personImage),
-                      ),
-                      title: Text(quote.quote, overflow: TextOverflow.ellipsis, softWrap: false,),
-                      subtitle: Text(quote.personName),
+                      title: Text('" ' + quoteList[index] + '"'),
                       trailing: IconButton(onPressed: () {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text('Quote Removed from Favorites'))
                         );
                       }, icon: const Icon(Icons.delete_forever)),
                       onTap: () {
-                        Navigator.of(context).pushNamed(ViewSingleQuote.routeName);
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (_) => ViewSingleQuote(quote: quoteList[index])
+                        ));
                       },
                     ),
                   );
